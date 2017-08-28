f569ef1991721607735cd36f007a5c7b4a9c2ab5
jordanv215 <jordanv215@gmail.com>
2017-08-27 23:15:00 -0600

    added set interval to script2

--- js/script2.js
+++ js/script2.js
@@ -1,2 +1,6 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
+
+//first parameter is function to be executed
+//second parameter indicates number of milliseconds before execution
+setInterval(function, milliseconds);
