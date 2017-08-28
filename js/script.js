b27f6b615c30a4319ff51a6cf3011393959380cf
jordanv215 <jordanv215@gmail.com>
2017-08-27 22:44:17 -0600

    added comments for my circle

--- js/script.js
+++ js/script.js
@@ -23,8 +23,12 @@ ctx.closePath(); //finish
 // time for a circle! break dem bricks yo
 ctx.beginPath();
 
+// this takes 6 parameters - x and y, which are the first 2
+  // the arc radius - 20 - positions is on stage (how big the circle is going to be)
+  // the 0 is the START angle. the math.PI*2 is the END angle
+    // math.PI*2 is equivilent to 360 degrees, and 0 is 0 degrees. The values can only be in radiuns though (hope I'm spelling that correctly)
+  // the false is there for the direction of the drawing. False means clockwise - which is default, but I'm putting it here just so I know in the future.
 ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-
 ctx.fillStyle = "green";
 
 ctx.fill();
