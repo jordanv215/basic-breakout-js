eb2b7345e59a17fca95c70ee2c079cbb01dab88b
jordanv215 <jordanv215@gmail.com>
2017-08-27 23:21:27 -0600

    now have a ball on the canvas, i'm now going to make it move

--- js/script2.js
+++ js/script2.js
@@ -5,6 +5,7 @@ var ctx = canvas.getContext('2d');
 //second parameter indicates number of milliseconds before execution
 setInterval(draw, 10);
 
+
 // so now the draw interval will be called every 10 seconds
 function draw() {
   // drawing code
@@ -13,5 +14,5 @@ function draw() {
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
-
+// ball will be redrawn on EVERY frame
 }
