c60f43577ef11965ddb073bcb17fa44ba87e75b0
jordanv215 <jordanv215@gmail.com>
2017-08-27 23:23:53 -0600

    added var x and y and changed the parameters

--- js/script2.js
+++ js/script2.js
@@ -5,12 +5,16 @@ var ctx = canvas.getContext('2d');
 //second parameter indicates number of milliseconds before execution
 setInterval(draw, 10);
 
+// this will center the ball horizontally since half of the width of the canvas is right in the middle
+var x = canvas.width/2;
+// vertically, we will push the ball up 30px from the bottom of the page
+var y = canvas.height - 30;
 
 // so now the draw interval will be called every 10 seconds
 function draw() {
   // drawing code
   ctx.beginPath();
-  ctx.arc(50, 50, 10, 0, Math.PI*2);
+  ctx.arc(x, y, 10, 0, Math.PI*2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
