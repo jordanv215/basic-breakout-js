c427e740b4eb77f506552fae6efb4a3c3c3ff56e
jordanv215 <jordanv215@gmail.com>
2017-08-27 23:16:13 -0600

    added draw function

--- js/script2.js
+++ js/script2.js
@@ -3,4 +3,10 @@ var ctx = canvas.getContext('2d');
 
 //first parameter is function to be executed
 //second parameter indicates number of milliseconds before execution
-setInterval(function, milliseconds);
+setInterval(draw, 10);
+
+
+function draw() {
+  // drawing code
+
+}
