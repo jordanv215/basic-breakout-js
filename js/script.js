959736c7e740882fb7851ef57ca3c4925c9fb633
jordanv215 <jordanv215@gmail.com>
2017-08-27 22:25:59 -0600

    added rect, color, and fill as a first shape. Lots of comments added for description

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@ var canvas = document.getElementById('myCanvas');
 // basically allows drawing on canvas!
 var ctx = canvas.getContext('2d');
 
-ctx.beginPath();
+ctx.beginPath(); //start
+//content goes between start and finish
 
-ctx.closePath();
+//rectangle - first two values specify the coordinates of the top left of the canvas
+// 2nd 2 values specify width and height
+ctx.rect(20, 40, 50, 50);
+
+//this style fills a color - in this case, red
+ctx.fillStyle = "#FF0000";
+
+// this actually paints the rectangle red.
+ctx.fill();
+ctx.closePath(); //finish
