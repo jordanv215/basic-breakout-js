bc40aec42ae45c3e9370971a778686ad81ae86f5
jordanv215 <jordanv215@gmail.com>
2017-08-27 22:22:34 -0600

    added ctx begin and close path

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ var canvas = document.getElementById('myCanvas');
 // returns drawing context on canvas - returns context identifier and context associated with the canvas - i'm using 2d because...well...it's breakout.
 // basically allows drawing on canvas!
 var ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+
+ctx.closePath();
