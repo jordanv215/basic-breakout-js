48d3e4459a0f2338fee52d7b62b7f041d36d5bb3
jordanv215 <jordanv215@gmail.com>
2017-08-28 12:37:54 -0600

    more framework

--- js/script2.js
+++ js/script2.js
@@ -23,7 +23,11 @@ function drawBall() {
 }
 
 function drawPaddle() {
-
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
 }
 
 function draw() {
