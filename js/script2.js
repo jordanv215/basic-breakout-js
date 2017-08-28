4128d91e53aa5448ea9694e2e59caa9f6881e4f5
jordanv215 <jordanv215@gmail.com>
2017-08-27 23:17:32 -0600

    added circle

--- js/script2.js
+++ js/script2.js
@@ -5,8 +5,13 @@ var ctx = canvas.getContext('2d');
 //second parameter indicates number of milliseconds before execution
 setInterval(draw, 10);
 
-
+// so now the draw interval will be called every 10 seconds
 function draw() {
   // drawing code
+  ctx.beginPath();
+  ctx.arc(50, 50, 10, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
 
 }
