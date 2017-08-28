4c167c81aa2db6e6d296f67cf9c9e197dfad8c75
jordanv215 <jordanv215@gmail.com>
2017-08-27 22:15:12 -0600

    added var for canvas and var for ctx - left comments to explain these

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// targets canvas
+var canvas = document.getElementById('myCanvas');
+
+// returns drawing context on canvas - returns context identifier and context associated with the canvas - i'm using 2d because...well...it's breakout.
+// basically allows drawing on canvas!
+var ctx = canvas.getContext('2d');
